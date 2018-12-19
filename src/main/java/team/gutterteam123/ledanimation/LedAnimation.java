cbd05ee94e2e78486cc4a42277bb7fce5865bcad
david <davidscandurra@gmail.com>
2018-12-19 23:14:38 +0100

    Starting Webserver

--- src/main/java/team/gutterteam123/ledanimation/LedAnimation.java
+++ src/main/java/team/gutterteam123/ledanimation/LedAnimation.java
@@ -3,11 +3,13 @@ package team.gutterteam123.ledanimation;
 import me.david.davidlib.application.Application;
 import me.david.davidlib.startup.BootContext;
 import me.david.webapi.WebApplicationType;
+import me.david.webapi.server.netty.NettyWebServer;
 
 public class LedAnimation extends Application implements WebApplicationType {
     @Override
     public void start(BootContext bootContext) throws Exception {
-
+        setWebServer(new NettyWebServer(this));
+        listen(5555);
     }
 
     @Override
