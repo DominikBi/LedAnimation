8308eb42818263e73754198501055b78330f288d
DominikBi <birkeldominik@gmail.com>
2019-02-14 16:20:52 +0100

    Testing ways for Animation

--- src/main/java/team/gutterteam123/ledanimation/elemets/Animation.java
+++ src/main/java/team/gutterteam123/ledanimation/elemets/Animation.java
@@ -3,8 +3,11 @@ package team.gutterteam123.ledanimation.elemets;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @Getter @Setter
 public class Animation {
     short id;
     String name;
+    ArrayList<Long> actions;
 }
